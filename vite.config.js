3993e768f5170ab134ce2466d2eef6196609a326
Arun V <91460627+ArunVijey@users.noreply.github.com>
2026-07-18 21:27:53 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// base must match the GitHub repo name for GitHub Pages
-export default defineConfig({
-  base: "/ather-log/",
-  plugins: [react(), tailwindcss()],
-});
